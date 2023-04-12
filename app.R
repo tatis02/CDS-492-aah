3deefee9376900a22e9d7cd30332870871a82a80
Annunziata Alvarez-cascos <117691041+tatis02@users.noreply.github.com>
2023-04-12 18:49:52 -0400

    Delete app.R

--- app.R
+++
@@ -1,7 +0,0 @@
-install.packages('rsconnect')
-rsconnect::setAccountInfo(name='tatis02',
-			  token='B7539D6DC941BFCCD6BEE2C97A13F1A0',
-			  secret='<SECRET>')
-        
- library(rsconnect)
-    rsconnect::deployApp('tatis02/tatis02.github.io')
